43d23ac5d88379c3ce526f0e7208111d45f0dc28
bara-samb <maambaara56@gmail.com>
2026-07-18 16:38:58 +0000

    fix(server): exporter l'application Express pour resoudre l'erreur d'adresse dans les tests

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,10 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Serveur démarré sur le port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+        console.log(`Serveur démarré sur le port ${PORT}`);
+    });
+}
+
+module.exports = app;
