0eb12e9a91b5d813ac5730441c1c03bfbae172c1
Mohamed <mbackem500@gmail.com>
2026-09-19 13:15:57 +0000

    ¨correction0¨

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -31,7 +31,7 @@ api.interceptors.response.use(
     if (error.response && error.response.status === 401) {
       localStorage.removeItem('token');
       localStorage.removeItem('user');
-      window.location.href = '/login';
+      window.loggin = window.location.href = '/login';
     }
     return Promise.reject(error);
   }
